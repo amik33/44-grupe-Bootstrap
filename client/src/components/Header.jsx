690cd5c8fa64b47f4b919d3d1e28bfc04fb63020
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-08-11 16:09:51 +0300

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,37 +7,44 @@ export function Header () {
     const userLoggedIn = ctx.user.loggedIn;
 
     const publicMenu = (
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-end">
-        <Link to="/login" className="btn btn-outline-light me-2">Login</Link>
-        <Link to="/register" className="btn btn-warning">Sign-up</Link>
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-end">
+          <Link to="/login" className="btn btn-outline-light me-2">Login</Link>
+          <Link to="/register" className="btn btn-warning">Sign-up</Link>
+        </div>
       </div>
     );
 
     const userMenu = (
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-end">
-        <Link to="/" className="d-flex align-items-right mb-2 mb-lg-0 text-white text-decoration-none">
-          <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
-        </Link>
+      <div className="container ">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-end">
+          <Link to="/" className="d-flex align-items-right mb-2 mb-lg-0 text-white text-decoration-none">
+            <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
+          </Link>
+        </div>
       </div>
     );
 
     return (
     <header className="p-3 text-bg-dark">
-    <div className="container">
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <Link to="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          {/* <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use to="#bootstrap"></use></svg> */}
-        </Link>
-        <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><Link to="/hero" className="nav-link px-2 text-secondary">Home</Link></li>
-          <li><Link to="/future" className="nav-link px-2 text-white">Features</Link></li>
-          <li><Link to="/products" className="nav-link px-2 text-white">Pricing</Link></li>
-          <li><Link to="/dashboard" className="nav-link px-2 text-white">Dashboard</Link></li>
-          <li><Link to="/content" className="nav-link px-2 text-white">Content</Link></li>
-          <li><Link to="/" className="nav-link px-2 text-white">About</Link></li>
-        </ul>
+      <div className="conteiner d-flex px-5">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <Link to="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+            {/* <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use to="#bootstrap"></use></svg> */}
+          </Link>
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+            <li><Link to="/hero" className="nav-link px-2 text-secondary">Home</Link></li>
+            <li><Link to="/future" className="nav-link px-2 text-white">Features</Link></li>
+            <li><Link to="/products" className="nav-link px-2 text-white">Pricing</Link></li>
+            <li><Link to="/dashboard" className="nav-link px-2 text-white">Dashboard</Link></li>
+            <li><Link to="/content" className="nav-link px-2 text-white">Content</Link></li>
+            <li><Link to="/" className="nav-link px-2 text-white">About</Link></li>
+            <li><Link to="/" className="nav-link px-2 text-white">About</Link></li>
+          </ul>
+        </div>
       </div>
-    </div>
-    {userLoggedIn ? userMenu : publicMenu}
-  </header>)
+      {userLoggedIn ? userMenu : publicMenu}
+      </div>
+    </header>)
 };
--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,6 +1,6 @@
 export function Products () {
     return ( 
-    <div className="container">
+    <div className="container pt-5">
         <div className="row row-cols-1 row-cols-md-3 g-4">
             <div className="col">
                 <div className="card h-100">
@@ -35,6 +35,78 @@ export function Products () {
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="Images"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="Images"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="Images"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a short card.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="Images"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="Images"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="Images"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="Images"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="col">
+                <div className="card h-100">
+                    <img src="..." className="card-img-top" alt="Images"/>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
                     </div>
                 </div>
             </div>
