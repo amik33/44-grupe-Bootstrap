b9537756ce62baff89168a32f19bf0552b0df513
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-08-15 14:54:15 +0300

    Create user dropdown meniu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 export function Header () {
     const ctx = useContext(UserContext);
     const userLoggedIn = ctx.user.loggedIn;
+    const [menuVisibility, setMenuVisibility] = useState(false);
 
     const publicMenu = (
       <div className="container">
@@ -18,10 +19,26 @@ export function Header () {
     const userMenu = (
       <div className="container ">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-end">
-          <Link to="/" className="d-flex align-items-right mb-2 mb-lg-0 text-white text-decoration-none">
+          <div onClick={() => setMenuVisibility(!menuVisibility)} className="d-flex align-items-right mb-2 mb-lg-0 text-white text-decoration-none">
             <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
-          </Link>
+          </div>
         </div>
+        <ul className={`dropdown-menu text-small ${ menuVisibility ? 'show' : ''}`} 
+          style={{
+              position: 'absolute', 
+              inset: '15px 63px auto auto',
+              margin: '0px', 
+              transform: 'translate3d(0.5px, 34px, 0px)',
+            }}>
+            <li><Link className="dropdown-item" to="/dashboard">Dashboard</Link></li>
+            <li><Link className="dropdown-item" to="/content">Content</Link></li>
+            <li><Link className="dropdown-item" to="/content/add">New content</Link></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><Link className="dropdown-item" href="/">Settings</Link></li>
+            <li><Link className="dropdown-item" href="/">Profile</Link></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><Link className="dropdown-item" to="/">Sign out</Link></li>
+        </ul>
       </div>
     );
 
--- client/src/pages/NoPages.jsx
+++ client/src/pages/NoPages.jsx
@@ -1,11 +1,11 @@
 
 export function NoPages () {
     return (
-        <div class="px-4 py-5 my-5 text-center">
-            {/* <img class="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/> */}
-                <h1 class="display-5 fw-bold text-body-emphasis">404</h1>
-                <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4">Page not found</p>
+        <div className="px-4 py-5 my-5 text-center">
+            {/* <img className="d-block mx-auto mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"/> */}
+                <h1 className="display-5 fw-bold text-body-emphasis">404</h1>
+                <div className="col-lg-6 mx-auto">
+                <p className="lead mb-4">Page not found</p>
             </div>
         </div>
     )
